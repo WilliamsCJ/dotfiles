f70f44f4353ee14e9e429bfa108f7dab4b8908a5
CJ Williams <cj@cjwilliams.io>
2021-12-28 21:54:23 +0000

    Updating install.sh to install oh-my-zsh spaceship prompt

--- install.sh
+++ install.sh
@@ -2,6 +2,30 @@
 
 set -e # -e: exit on error
 
+if ! $CODESPACES ; then
+  echo "Not a GitHub Codespaces environment"
+else
+  echo "GitHub Codespaces environment"
+fi
+
+# Install oh-my-zsh
+if ! $CODESPACES ; then
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  else
+    echo "To install ohmyzsh, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
+
+# Install spaceship prompt for zsh
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
