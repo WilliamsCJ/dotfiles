9aa532c9c3266eb118fbb91e2ad23c161abfb2b1
CJ Williams <cj@cjwilliams.io>
2022-09-10 19:31:58 +0100

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -3,6 +3,7 @@ eval "open https://github.com/arcticicestudio/nord-terminal-app" # Nord terminal
 eval "open https://www.jetbrains.com/lp/mono/" # JetBrains Mono font
 eval "open https://www.macbartender.com" # Bartender
 eval "open https://linear.app/download" # Linear issue tracking
+eval "open https://klog.jotaen.net" # Klog time tracking
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
