8c38277b28c5e2f44255888721da34cf1084a870
CJ Williams <cj@cjwilliams.io>
2022-09-10 18:26:29 +0100

    Update install-macos.sh to install Homebrew

--- install-macos.sh
+++ install-macos.sh
@@ -4,3 +4,5 @@ eval "open https://www.jetbrains.com/lp/mono/" # JetBrains Mono font
 eval "open https://www.macbartender.com" # Bartender
 eval "open https://www.pock.app" # Pock touchbar app
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
