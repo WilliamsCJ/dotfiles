00cd1eb4d416954373658f197d8d8d61704799c9
CJ Williams <cj@cjwilliams.io>
2022-09-11 15:01:03 +0100

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -1,11 +1,3 @@
-# Open links to download things
-eval "open https://github.com/arcticicestudio/nord-terminal-app" # Nord terminal theme
-eval "open https://www.jetbrains.com/lp/mono/" # JetBrains Mono font
-eval "open https://www.macbartender.com" # Bartender
-eval "open https://linear.app/download" # Linear issue tracking
-eval "open https://klog.jotaen.net" # Klog time tracking
-eval "open https://www.raycast.com" # Raycast launcher
-
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -14,3 +6,11 @@ export CODESPACES=false
 
 # Run install script
 eval "./install.sh"
+
+# Open links to download things
+eval "open https://github.com/arcticicestudio/nord-terminal-app" # Nord terminal theme
+eval "open https://www.jetbrains.com/lp/mono/" # JetBrains Mono font
+eval "open https://www.macbartender.com" # Bartender
+eval "open https://linear.app/download" # Linear issue tracking
+eval "open https://klog.jotaen.net" # Klog time tracking
+eval "open https://www.raycast.com" # Raycast launcher
